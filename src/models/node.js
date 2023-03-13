39ba4b84a7cc4deb3086777ce65b533f91a7a362
AbdulHannan-123 <ah6475774@gmail.com>
2023-03-14 00:32:57 +0500

    design schema for the app and clean the code

---
+++ src/models/node.js
@@ -0,0 +1,35 @@
+// create the mongoose model you need to follow the few steps
+// 1. define scheme
+// 2. create model -> model requirs 2 things <Model_name>, <Scheme> 
+
+const mongoose = require('mongoose');
+
+const mongoSchema = mongoose.Schema({    // you need to pass a map/obj in the argument of schema
+     // now you need to tell what happens here
+     id:{   // giving some details to id
+        type : String,        // declares its data type
+        unique : true,        //is must be unique ristrict duplications  
+        required : true        // its nesseory
+     },
+     userid :{
+        type : String,
+        required : true
+     },
+     userid :{
+        type : String,
+        required : true
+     },
+     title :{
+        type : String,
+        required : true
+     },
+     content :{
+        type : String,
+     },
+     dateadded :{
+        type : Date,
+        default : Date.now
+     }
+
+
+});
