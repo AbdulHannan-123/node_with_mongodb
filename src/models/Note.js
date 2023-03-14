0f2c9cfbfb725c9b51c52c8232d07cb81c4c2e68
AbdulHannan-123 <ah6475774@gmail.com>
2023-03-14 23:23:24 +0500

    sending the risponse to db and fetching

--- src/models/Note.js
+++ src/models/Note.js
@@ -4,32 +4,35 @@
 
 const mongoose = require('mongoose');
 
-const mongoSchema = mongoose.Schema({    // you need to pass a map/obj in the argument of schema
+const noteSchema = mongoose.Schema({    // you need to pass a map/obj in the argument of schema
      // now you need to tell what happens here
      id:{   // giving some details to id
         type : String,        // declares its data type
         unique : true,        //is must be unique ristrict duplications  
-        required : true        // its nesseory
+        required : true,        // its nesseory
      },
      userid :{
         type : String,
-        required : true
+        required : true,
      },
      userid :{
         type : String,
-        required : true
+        required : true,
      },
      title :{
         type : String,
-        required : true
+        required : true,
      },
      content :{
         type : String,
      },
      dateadded :{
         type : Date,
-        default : Date.now
+        default : Date.now,
      }
-
-
 });
+
+// lets create the model after schema
+
+ module.exports= mongoose.model("Note", noteSchema);   // Note k naam se ye model ab availabel hojaega hr jagah but before that you neet to export
+
--- src/server_2.js
+++ src/server_2.js
@@ -4,16 +4,35 @@
 const express = require('express');   //we call the the express and stoer it in a variable   OR we call the instance
 const app = express();    // we call express as function  and stroe it in app      // express me server bn chuka ha
 
+const Note = require('./models/Note');
+
 const mongoose = require('mongoose');   // by using this i can connect the my mongoDB
 
 mongoose.connect('mongodb+srv://ali:ali123@cluster0.xtv74tc.mongodb.net/notesdb')     // need to pass the connection string
     .then(function () {     // when connection success then show these pages outher wise dont
         app.get("/", function (req, res) {
             res.send("This is the homepage_2");
+        });   
+
+        app.get("/notes/list", async function (req, res) {
+            var notes = await Note.find();   // find all type Note in db
+            res.json(notes);
         });
 
-        app.get("/notes", function (req, res) {
-            res.send("This is the nodespage");
+        app.get("/notes/add", async function (req, res) {
+
+            const newNote = new Note({
+                id: "0001",
+                userid: "hannan@gmail.com",
+                title: " my node",
+                content: "this is content"
+
+            });
+            await newNote.save();
+
+            const response = {message: "new node created"};
+
+            res.json(response);
         });
     });
 
